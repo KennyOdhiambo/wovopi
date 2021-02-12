5201d349aef7403265212d5d923d5ca2852bbd5f
Kennedy Odhiambo <oduorkennedy@gmail.com>
2021-02-12 16:10:22 +0300

    Recommened changes

---
+++ script.js
@@ -0,0 +1,14 @@
+// Get the container element
+var btnContainer = document.getElementById("navbar-nav ml-auto");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("nav-item");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+    nav - item[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
